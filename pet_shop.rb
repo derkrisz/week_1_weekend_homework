1c208bf667b64f012eb731c4b2fb06e1ce2a1221
derkrisz <krisztian.dergmail.com>
2017-11-05 10:04:46 +0000

    passes test stock count

--- pet_shop.rb
+++ pet_shop.rb
@@ -17,3 +17,7 @@ end
 def increase_pets_sold(pet_shop, num)
   return pet_shop[:admin][:pets_sold] += num #maybe call the pets_sold method??
 end
+
+def stock_count(pet_shop)
+  return pet_shop[:pets].count
+end
