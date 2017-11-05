877de7122e0de7663f24839f716962957b2d6a3c
derkrisz <krisztian.dergmail.com>
2017-11-05 15:11:36 +0000

    passes tests_all_pets_by_breed

--- pet_shop.rb
+++ pet_shop.rb
@@ -11,13 +11,23 @@ def add_or_remove_cash(pet_shop, amount)
 end
 
 def pets_sold(pet_shop)
-  return pet_shop[:admin][:pets_sold]
+  pet_shop[:admin][:pets_sold]
 end
 
 def increase_pets_sold(pet_shop, num)
-  return pet_shop[:admin][:pets_sold] += num #maybe call the pets_sold method??
+  return pet_shop[:admin][:pets_sold] += num #how to call previous function?
 end
 
 def stock_count(pet_shop)
   return pet_shop[:pets].count
 end
+
+def pets_by_breed(pet_shop, breed)
+  result_pets = []
+  for pet in pet_shop[:pets]
+    if pet[:breed] == breed
+      result_pets.push(pet)
+    end
+  end
+  return result_pets
+  end
